2a4696a05428952d08864ea9e776ca3f61a38d38
create-stree <kirsiacb@gmail.com>
2025-07-18 08:34:11 +0900

    Brainrot.lua

--- Brainrot.lua
+++ Brainrot.lua
@@ -5,14 +5,13 @@ local Window = OrionLib:MakeWindow({
     HidePremium = true,
     SaveConfig = true,
     ConfigFolder = "STREE HUB",
-    Icon = "123032091977400",
+    Icon = "rbxassetid://123032091977400",
     IntroEnabled = true,
     IntroText = "Welcome To Script STREE HUB",
-    Theme = Dark,
+    Theme = "Dark", -- Perlu tanda kutip
     CloseCallback = function()
         print("UI Closed!")
-    end
-
+    end, -- Tambahkan koma di sini
     ToggleUIKeybind = "K",
     DisableRayfieldPrompts = false,
     DisableBuildWarnings = false,
@@ -25,7 +24,7 @@ local Window = OrionLib:MakeWindow({
 
     Discord = {
         Enabled = true,
-        Invite = "MFzWcQNA", -- hanya code
+        Invite = "MFzWcQNA", -- hanya kode invite
         RememberJoins = true
     },
 
@@ -37,48 +36,20 @@ local Window = OrionLib:MakeWindow({
         FileName = "Key",
         SaveKey = false,
         GrabKeyFromSite = false,
-        Key = {"PASTE_KEY_HERE", "PASTEKEYHERE","StreeHubScriptInIndonesia"}
+        Key = {"PASTE_KEY_HERE", "PASTEKEYHERE", "StreeHubScriptInIndonesia"}
     }
 })
 
---[[
-Name = <string> - The name of the UI.
-HidePremium = <bool> - Whether or not the user details shows Premium status or not.
-SaveConfig = <bool> - Toggles the config saving in the UI.
-ConfigFolder = <string> - The name of the folder where the configs are saved.
-IntroEnabled = <bool> - Whether or not to show the intro animation.
-IntroText = <string> - Text to show in the intro animation.
-IntroIcon = <string> - URL to the image you want to use in the intro animation.
-Icon = <string> - URL to the image you want displayed on the window.
-CloseCallback = <function> - Function to execute when the window is closed.
-]]
-
+-- Tab Home
 local HomeTab = Window:MakeTab({
     Name = "Home",
     Icon = "rbxassetid://124242667284964",
     PremiumOnly = false
 })
 
-
-
---[[
-Name = <string> - The name of the tab.
-Icon = <string> - The icon of the tab.
-PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
-]]
-
+-- Tab Game
 local UniversalTab = Window:MakeTab({
     Name = "game",
     Icon = "rbxassetid://453473360",
     PremiumOnly = false
 })
-
---[[
-Name = <string> - The name of the tab.
-Icon = <string> - The icon of the tab.
-PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
-]]
-
-OrionLib:Destroy()
-
-OrionLib:Init()
